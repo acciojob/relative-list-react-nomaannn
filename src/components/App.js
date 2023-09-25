d73ff82b35136c75ec1131d48e175b00796afa0c
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-09-25 16:25:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ const App = () => {
   return (
     <div id="main">
                <ol key="relativeList">
-                <li key="relativeListItem1">Amit</li>
-                <li key="relativeListItem2">Suman</li>
-                <li key="relativeListItem3">Rajesh</li>
-                <li key="relativeListItem4">Priya</li>
-                <li key="relativeListItem5">Neeta</li>
+                <li key="Amit">Amit</li>
+                <li key="Suman">Suman</li>
+                <li key="Rajesh">Rajesh</li>
+                <li key="Priya">Priya</li>
+                <li key="Neeta">Neeta</li>
 </ol>
     </div>
   )
